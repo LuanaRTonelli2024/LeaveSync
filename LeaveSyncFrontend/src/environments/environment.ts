83259be969b045903121d2953ad81f93b9b78bd2
LuanaRTonelli <luana_tonelli@hotmail.com>
2026-05-01 22:11:28 -0400

    Environment: Render

--- LeaveSyncFrontend/src/environments/environment.ts
+++ LeaveSyncFrontend/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
-  apiUrl: 'http://localhost:5000'
+  apiUrl: 'https://leavesyncbackend-iblr.onrender.com'
 };
 
 // apiUrl: 'AQUI VA EL LINK DE RENDER UNA VEZ DE HAGA EL DEPLOY'
+//apiUrl: 'http://localhost:5000'
